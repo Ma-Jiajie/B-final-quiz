3f373b86475ac822cfa000a1176e494c7f0ac9f8
Jiajie <jiajie.ma@thoughtworks.com>
2020-10-29 11:39:46 +0800

    [jiajie]add Trainee entity and application.properties

---
+++ src/main/java/com/example/demo/model/Trainee.java
@@ -0,0 +1,33 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Trainee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "name should not be empty")
+    private String name;
+    @JsonIgnore
+    private Boolean grouped;
+}
