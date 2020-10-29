51b2b493917ec464f24437a530f1d757d32afa6d
Jiajie <jiajie.ma@thoughtworks.com>
2020-10-29 14:56:23 +0800

    [jiajie]init Group function

---
+++ src/main/java/com/example/demo/controller/GroupController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/trainers")
+@Validated
+public class GroupController {
+
+}
