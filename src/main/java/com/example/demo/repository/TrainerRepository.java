f977ea30db3f4294b03403dd572ec22707ad8f48
Jiajie <jiajie.ma@thoughtworks.com>
2020-10-29 14:12:29 +0800

    [jiajie]add trainer Repository

---
+++ src/main/java/com/example/demo/repository/TrainerRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Trainer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.List;
+
+public interface TrainerRepository extends JpaRepository<Trainer,Long> {
+    List<Trainer> getAllByGrouped(Boolean grouped);
+    Trainer getOneById(Long id);
+}
